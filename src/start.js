a7a41e4bd5e10bcb3aec7ac6f1af64e01b8d2112
kiranjd <kiranjd8@gmail.com>
2020-01-04 16:31:50 +0530

    updated all critical packages

--- src/start.js
+++ src/start.js
@@ -1,31 +1,33 @@
-import {MongoClient, ObjectId} from 'mongodb'
-import express from 'express'
-import bodyParser from 'body-parser'
-import {graphqlExpress, graphiqlExpress} from 'graphql-server-express'
-import {makeExecutableSchema} from 'graphql-tools'
-import cors from 'cors'
-import {prepare} from "../util/index"
+import { MongoClient, ObjectId } from "mongodb";
+import express from "express";
+import bodyParser from "body-parser";
+import { graphqlExpress, graphiqlExpress } from "graphql-server-express";
+import { makeExecutableSchema } from "graphql-tools";
+import cors from "cors";
+import { prepare } from "../util/index";
 
+const app = express();
 
-const app = express()
+app.use(cors());
 
-app.use(cors())
-
-const homePath = '/graphiql'
-const URL = 'http://localhost'
-const PORT = 3001
-const MONGO_URL = 'mongodb://localhost:27017/blog'
-
-
+const homePath = "/graphiql";
+const URL = "http://localhost";
+const PORT = 3001;
+const MONGO_URL = "mongodb://localhost:27017";
 
 export const start = async () => {
   try {
-    const db = await MongoClient.connect(MONGO_URL)
+    const client = await MongoClient.connect(MONGO_URL, {
+      useUnifiedTopology: true
+    });
 
-    const Posts = db.collection('posts')
-    const Comments = db.collection('comments')
+    const db = client.db("blog");
 
-    const typeDefs = [`
+    const Posts = db.collection("posts");
+    const Comments = db.collection("comments");
+
+    const typeDefs = [
+      `
       type Query {
         post(_id: String): Post
         posts: [Post]
@@ -55,61 +57,61 @@ export const start = async () => {
         query: Query
         mutation: Mutation
       }
-    `];
+    `
+    ];
 
     const resolvers = {
       Query: {
-        post: async (root, {_id}) => {
-          return prepare(await Posts.findOne(ObjectId(_id)))
+        post: async (root, { _id }) => {
+          return prepare(await Posts.findOne(ObjectId(_id)));
         },
         posts: async () => {
-          return (await Posts.find({}).toArray()).map(prepare)
+          return (await Posts.find({}).toArray()).map(prepare);
         },
-        comment: async (root, {_id}) => {
-          return prepare(await Comments.findOne(ObjectId(_id)))
-        },
+        comment: async (root, { _id }) => {
+          return prepare(await Comments.findOne(ObjectId(_id)));
+        }
       },
       Post: {
-        comments: async ({_id}) => {
-          return (await Comments.find({postId: _id}).toArray()).map(prepare)
+        comments: async ({ _id }) => {
+          return (await Comments.find({ postId: _id }).toArray()).map(prepare);
         }
       },
       Comment: {
-        post: async ({postId}) => {
-          return prepare(await Posts.findOne(ObjectId(postId)))
+        post: async ({ postId }) => {
+          return prepare(await Posts.findOne(ObjectId(postId)));
         }
       },
       Mutation: {
         createPost: async (root, args, context, info) => {
-          const res = await Posts.insertOne(args)
-          return prepare(res.ops[0])  // https://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#~insertOneWriteOpResult
+          const res = await Posts.insertOne(args);
+          return prepare(res.ops[0]); // https://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#~insertOneWriteOpResult
         },
         createComment: async (root, args) => {
-          const res = await Comments.insert(args)
-          return prepare(await Comments.findOne({_id: res.insertedIds[1]}))
-        },
-      },
-    }
+          const res = await Comments.insert(args);
+          return prepare(await Comments.findOne({ _id: res.insertedIds[1] }));
+        }
+      }
+    };
 
     const schema = makeExecutableSchema({
       typeDefs,
       resolvers
-    })
+    });
 
+    app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 
-    app.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
-
-
-    app.use(homePath, graphiqlExpress({
-      endpointURL: '/graphql'
-    }))
+    app.use(
+      homePath,
+      graphiqlExpress({
+        endpointURL: "/graphql"
+      })
+    );
 
     app.listen(PORT, () => {
-      console.log(`Visit ${URL}:${PORT}${homePath}`)
-    })
-
+      console.log(`Visit ${URL}:${PORT}${homePath}`);
+    });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-
-}
+};
